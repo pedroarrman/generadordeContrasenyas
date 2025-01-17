2e911e288d5579bccdaf0f57cdfcab32a8ba3479
pedro arribas <104268930+pedroarrman@users.noreply.github.com>
2025-01-17 21:31:09 +0100

    Add files via upload

---
+++ fecha.sh
@@ -0,0 +1,16 @@
+# !/BIN/BASH
+fecha=`date +%d`
+if [ $fecha == 25 ]
+then
+	echo "FU FU FU"
+else
+	echo "ponte a estudiar que falta te hace"
+fi
+
+ctrl+x+k
+
+fecha.sh
+
+chmod 777 fecha.sh
+
+./fecha.sh
